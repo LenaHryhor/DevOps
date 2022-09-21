a4d467ff8ab57d321ebdb37d759516e0765d87d9
Olena Hryhor <hrigor21@gmail.com>
2022-09-22 00:10:12 +0300

    lab1

---
+++ lab1/hryhor/tests/lena-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m hryhor -p lena [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "lena"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
